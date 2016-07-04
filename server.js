87d9721dcabe43477ab605b714547bd9edb62400
Prommin <crazyfox_pr@hotmail.com>
2016-07-05 02:23:39 +0700

    fix PORT for heroku

--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.use('/', express.static('./public'));
 app.get('/',function (req,res) {
     res.sendFile('./index.html')
 })
-
-app.use(function (req,res) {
-  if (req.url === '/favicon.ico') {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-    res.end();
-    return;
-  }
+app.use(function(req, res, next){
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end(/* icon content here */);
+    } else {
+        next();
+    }
 })
 
 app.use(function (req,res,next) {
